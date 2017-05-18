8a4a3fd1136d4fa27b11a46a9eb46151084886ba
Kornelije Petak <kpetak@koncar-inem.hr>
2017-05-18 06:16:47 +0200

    Add Settings and DTE2 singleton instance via VS.Instance

---
+++ Horizon/Horizon/Settings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace Horizon
+{
+	public class Setting<T>
+	{
+		private string path;
+
+		public Setting(string path)
+		{
+			this.path = path;
+		}
+
+		public T Value
+		{
+			get
+			{
+				return Settings.GetValue<T>(path);
+			}
+			set
+			{
+				Settings.SetValue(path, value);
+			}
+		}
+
+	}
+
+	public static class Settings
+	{
+		public static T GetValue<T>(string path, T defaultValue = default(T))
+		{
+			try
+			{
+				dynamic val = getProperty(path).Value;
+				return (T)val;
+			}
+			catch
+			{
+				return defaultValue;
+			}
+		}
+
+		public static bool SetValue<T>(string path, T value)
+		{
+			try
+			{
+				getProperty(path).Value = value;
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+
+		private static Property getProperty(string path)
+		{
+			string[] parts = path.Split('.');
+			string category = parts[0];
+			string page = parts[1];
+			string property = parts[2];
+
+			Properties props = VS.Instance.get_Properties(category, page);
+			return props.Item(property);
+		}
+
+	}
+}
---
+++ Horizon/Horizon/VS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace Horizon
+{
+	public static class VS
+	{
+		public static DTE2 Instance
+			=> ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2;
+	}
+}
